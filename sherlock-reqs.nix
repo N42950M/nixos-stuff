0a5ca561fb27a6e15528b6da88dbb01d9734f331
= <n42950m@proton.me>
2024-02-27 11:15:51 -0500

    updated reqs shell

--- sherlock-reqs.nix
+++ sherlock-reqs.nix
@@ -5,7 +5,7 @@ in pkgs.mkShell {
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.certifi
       python-pkgs.colorama
-      python-pkgs.PySocks
+      python-pkgs.pysocks
       python-pkgs.requests
       python-pkgs.requests-futures
       python-pkgs.stem
