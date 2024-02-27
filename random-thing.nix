1ace9099e9f6540cdf3c24998d663bc76edd1b79
= <n42950m@proton.me>
2024-02-27 10:59:50 -0500

    random python shell for thing yeah

---
+++ random-thing.nix
@@ -0,0 +1,18 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.certifi
+      python-pkgs.colorama
+      python-pkgs.PySocks
+      python-pkgs.requests
+      python-pkgs.requests-futures
+      python-pkgs.stem
+      python-pkgs.torrequest
+      python-pkgs.pandas
+      python-pkgs.openpyxl
+      python-pkgs.exrex
+    ]))
+  ];
+}
